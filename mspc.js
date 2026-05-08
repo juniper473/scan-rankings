2a8a82b584300ee23778d089ae0a109b285e5948
Ms.Swan <hyrulezelda504@gmail.com>
2026-05-08 16:22:41 +0530

    Update mspc.js

--- mspc.js
+++ mspc.js
@@ -28,7 +28,7 @@ const scripts = [
   { name: 'Boyfriend Kiss', fn: runBoyfriendKiss, alwaysRun: true },
   { name: 'Guild Show', fn: runGuildShow, alwaysRun: true },
   { name: 'Pet Training', fn: runPetTraining, alwaysRun: false },
-  { name: 'Bridesmaids Tasks', fn: runBridesmaids, alwaysRun: true },
+  { name: 'Bridesmaids Tasks', fn: runBridesmaids, alwaysRun: false },
 ];
 
 (async () => {
