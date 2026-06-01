0aab5966f75541f8e439f4b4aa5cbf6100318fca
Ms.Swan <hyrulezelda504@gmail.com>
2026-06-02 00:00:23 +0530

    Update bridesmaids.js

--- bridesmaids.js
+++ bridesmaids.js
@@ -2,7 +2,7 @@ module.exports = async function runBridesmaids(page) {
   // =========================
   // 🔧 CONFIG
   // =========================
-  const PARTY_ID = 10303;
+  const PARTY_ID = 11283;
   const TARGET_URL =
     `https://v3.g.ladypopular.com/party/center/planning.php?bridesmaid_party_id=${PARTY_ID}`;
 
